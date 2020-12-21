a4597bacd13d82c4f52d7fbd8b3460f4f60ce6c5
Matthijs van Oostende <matthijsvanoostende@hotmail.com>
2020-12-21 12:16:12 +0100

    Add Twig and Security check Recipes

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/rrr")
@@ -24,6 +25,9 @@ class QuestionController
     {
         $content = "To do...." . $vraag;
 
-        return new Response($content);
+        return $this->render('question/show.html.twig', [
+           'text' => $content,
+        ]);
+
     }
 }
