b1e7a7e472cb8d3be929ab4c820fa338a2c469fc
Matthijs van Oostende <matthijsvanoostende@hotmail.com>
2020-12-26 22:36:35 +0100

    Add and implement jquery and bootstrap as node_modules

--- assets/app.js
+++ assets/app.js
@@ -8,9 +8,10 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-
 // start the Stimulus application
 import './bootstrap';
+
+import $ from 'jquery';
 
 var $container = $('.js-vote-arrows');
 $container.find('a').on('click', function(e) {
