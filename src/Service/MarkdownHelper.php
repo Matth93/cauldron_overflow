d53ad9111de956d05ebc9ce2dc9133d0612b8409
Matthijs van Oostende <matthijsvanoostende@hotmail.com>
2020-12-27 15:20:15 +0100

    Disable caching with debug off

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -24,6 +24,10 @@ class MarkdownHelper
 
     public function parse(string $source):string
     {
+        if ($this->isDebug) {
+            return $this->markdownParser->transformMarkdown($source);
+        }
+
         return $this->cache->get('markdown_'.md5($source), function() use ($source) {
             return $this->markdownParser->transformMarkdown($source);
         });
