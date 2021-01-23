8ce7e97c64d74f0bdb205e4f171530e0f30a3c0d
Matthijs van Oostende <matthijsvanoostende@hotmail.com>
2021-01-23 17:31:55 +0100

    Create comments after submitting

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,10 +4,13 @@
 namespace App\Controller;
 
 
+use App\Entity\Comment;
 use App\Form\CommentFormType;
+use App\Repository\QuestionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
@@ -32,9 +35,28 @@ class CommentController extends AbstractController
     /**
      * @Route("/comment/new", name="comment_new")
      */
-    public function new(EntityManagerInterface $entityManager)
+    public function new(EntityManagerInterface $entityManager,Request $request, QuestionRepository $questionRepository)
     {
         $form = $this->createForm(CommentFormType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $comment = new Comment();
+            $comment->setAuthorName($data['authorName']);
+            $comment->setContent($data['content']);
+            $comment->setIsDeleted(false);
+
+            $questions = $questionRepository->findAll();
+            shuffle($questions);
+            $randomQuestion = $questions[0];
+            $comment->setQuestion($randomQuestion);
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('comment_admin');
+        }
 
         return $this->render('comment/new.html.twig', [
            'commentForm' => $form->createView()
